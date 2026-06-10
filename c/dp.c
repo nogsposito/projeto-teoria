4186e2df57a6dba4ea6dee976d24a5298d732858
nogsposito <viniciussposito3103@gmail.com>
2026-06-10 10:22:22 -0300

    correção no csv

--- c/dp.c
+++ c/dp.c
@@ -7,16 +7,16 @@ bool subset_sum_dp(int conjunto[], int n, int alvo){
     // alocação dinâmica
     bool **dp = (bool **)malloc((n+1) * sizeof(bool *));
     for (int i = 0; i <= n; i++){
-        dp[i][0] = (bool *)malloc((alvo + 1) * sizeof(bool));
+        dp[i] = (bool *)malloc((alvo + 1) * sizeof(bool));
     }
 
     for (int i = 0; i <= n; i++) {
-        dp[i][0] = true;  // IMPORTANTE: Adicione o  aqui
+        dp[i][0] = true;  // CORREÇÃO: dp na linha i, coluna 0
     }
 
     // 2. Se o alvo > 0 e o conjunto está vazio (linha 0), a resposta é falsa
     for (int j = 1; j <= alvo; j++) {
-        dp[0][j] = false; // IMPORTANTE: Adicione o  no início e [j] depois
+        dp[0][j] = false; // CORREÇÃO: dp na linha 0, coluna j
     }
     
     // preenchendo a tabela dp
